deb299e3efde9faf42de6d8cb8c6c8093f6e3f26
Wahid Niam <iam.wahidn@gmail.com>
2022-03-06 09:45:20 +0700

    update format

--- parameters.py
+++ parameters.py
@@ -19,6 +19,10 @@ def get_prime_numbers(x, y):
 
     return result
 
+
+def format_byte(val):
+    return '{0:016b}'.format(val)
+
 # get prime number until 100
 # the bigger the number, the more time it takes
 prime_list = get_prime_numbers(0, 100)
@@ -30,18 +34,6 @@ prime_list = [x for x in prime_list if x != q]
 
 p = random.choice(prime_list)
 
-# byte format of q and p, if needed
-q_16b = '{0:016b}'.format(q)
-p_16b = '{0:016b}'.format(p)
-
-
-def compute_N(_p, _q):
-    result = _p * _q
-    return result, '{0:016b}'.format(result)
-
-def compute_Phi_N(_p, _q):
-    result = (_p - 1) * (_q - 1)
-    return result, '{0:016b}'.format(result)
 
 def computegcd(e, Phi_N):
     if Phi_N == 0:
@@ -49,10 +41,7 @@ def computegcd(e, Phi_N):
     else:
         return computegcd(Phi_N, e % Phi_N)
 
-N, N16 = compute_N(p, q)
-Phi_N, Phi_N16 = compute_Phi_N(p, q)
-
-def get_public_key(Phi_N):
+def get_e(Phi_N):
     e = random.randint(1, Phi_N)
     gcd = computegcd(e, Phi_N)
     while gcd != 1:
@@ -61,22 +50,25 @@ def get_public_key(Phi_N):
     
     return e
 
-e = get_public_key(Phi_N)
 
-def get_private_key(e, Phi_N):
+def get_d(e, Phi_N):
     d = 1
     while d * e % Phi_N != 1:
         d += 1
     return d
 
-d = get_private_key(e, Phi_N)
+N = p * q
+Phi_N = (p - 1) * (q - 1)
+
+e = get_e(Phi_N)
+d = get_d(e, Phi_N)
 
 # print all the parameters
 def show_parameters():
-    print("p: {}".format(p))
-    print("q: {}".format(q))
-    print("N: {}".format(N))
-    print("Phi(N): {}".format(Phi_N))
-    print("public key e: {}".format(e))
-    print("private key d: {}".format(d))
-    print("N, e: {}, {}".format(N, e))
+    print("p: {}, {}".format( p, format_byte(p) ))
+    print("q: {}, {}".format( q, format_byte(q) ))
+    print("N: {}, {}".format( N, format_byte(N) ))
+    print("Phi(N): {}, {}".format( Phi_N, format_byte(Phi_N) ))
+    print("public key e: {}, {}".format( e, format_byte(e) ))
+    print("private key d: {}, {}".format( d, format_byte(d) ))
+    print("N, e: {} ({}), {} ({})".format( N, format_byte(N), e, format_byte(e) ))
